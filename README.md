f795d951dc04a8d37f93b089fe6621d2f0819868
DevCoff <105569223+DevCoff@users.noreply.github.com>
2023-09-26 00:47:04 +0000

    adicionado script e

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Projeto Dev.curso
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
 function Home() {
-    return <h1>Amor! Tenha um bom dia ♥ </h1>
-} 
+  return <h1>Amor! Tenha um bom dia ♥ </h1>;
+}
 export default Home;
